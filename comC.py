97df0c2de3c757ce0030a6bc7baa54179053d5b1
siddhav <siddharth.verma29.93@gmail.com>
2017-04-19 20:30:41 -0400

    Add files via upload

---
+++ comC.py
@@ -0,0 +1,49 @@
+import sys, random, math, pygame
+from pygame.locals import *
+from math import sqrt,cos,sin,atan2
+from lineIntersect import *
+
+numNodes = 10
+
+class Node:
+    x = 0
+    y = 0
+    cost=0
+    parent=None
+    def __init__(self,xcoord, ycoord):
+         self.x = xcoord
+         self.y = ycoord
+
+def dist(xy1,xy2):
+    return sqrt((xy1[0]-xy2[0])*(xy1[0]-xy2[0])+(xy1[1]-xy2[1])*(xy1[1]-xy2[1]))
+
+def main():
+    nodes = []
+    nodes.append(Node(0.0,0.0))
+    startnode = nodes[0]
+    goalnode = Node(500.0,500.0)
+    for i in range(numNodes):
+        #voronoi section
+        #ends with giving a point (x,y)
+        xpt = 0
+        ypt = 0
+        vnode = Node(xpt,ypt)
+        cnode = nodes[0] #start from the start node
+        #find closest node to the selected voronoi node
+        for n in nodes:
+            if (dist([n.x,n.y],[vnode.x,vnode.y]) < dist([cnode.x,cnode.y],[vnode.x,vnode.y])):
+                cnode = n
+        #interpolation towards the voronoi node from the closest node
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+    '''running = True
+    while running:
+       for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+                 running = False'''
