e6f343de5aba44de8607587fb13bd811b99ecfd9
Sangha8291 <sahirra@g.clemson.edu>
2017-04-27 18:52:48 -0400

    Add files via upload

---
+++ gridfunction.py
@@ -0,0 +1,11 @@
+print('grid creation start here')
+grid=[]
+a12=[]
+for a in range(0,10):
+    a12.append(0)
+for b in range(0,10):
+    grid.append(a12)
+
+l=len(grid)
+for a in range (0,l):
+    print(grid[a])
