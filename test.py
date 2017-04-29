24b110999b5f9c629d50bb4da4c6a7218b777a8a
siddhav <siddharth.verma29.93@gmail.com>
2017-04-28 21:55:48 -0400

    now only have to import one function from the astar_siddhav.py module

--- test.py
+++ test.py
@@ -9,12 +9,14 @@ gstart = [6,16] #there is a function to calculate this in the astar_siddhav.py c
 start = [4, 3, 2] #[grid row, grid col, direction]
 goal = [6, 18]
 points,plan = astar(start,grid,obs,goal)
-print("Printing from test file")
+'''print("Printing from test file")
 for pr in range(len(plan)):
     print(plan[pr])
-print(points)
+print(points)'''
 #adding code for ghost
-'''
+def dotp(x,y):
+    z = x[0]*y[0] + x[1]*y[1]
+    return z
 #sensing distance
 sens = 3
 #Parameters for force
@@ -22,9 +24,12 @@ kpar = 1.5
 mpar = 2
 t0par = 3
 epspar = 0
+radius = 0.5
+prefspeed = 1.2
+maxspeed = 1.6
 pos = gstart
 vel = [0,0]
 goal = [5,6] #this will be the current position of the snake
-gvel = goal-pos
-gvel = gvel/(sqrt(gvel.dot(gvel)))*prefspeed
-ghost = [pos,vel,gvel,goal,radius,prefspeed,maxspeed]'''
+gvel = [goal[0]-pos[0],goal[1]-pos[1]]
+gvel = [gvel[0]/(sqrt(dotp(gvel,gvel)))*prefspeed,gvel[1]/(sqrt(dotp(gvel,gvel)))*prefspeed]
+ghost = [pos,vel,gvel,goal,radius,prefspeed,maxspeed]
