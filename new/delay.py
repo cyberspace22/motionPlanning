e820066d5ebbefed563907b7b3a8710e4823e072
cyberspace22 <cyberspace93@gmail.com>
2017-04-30 13:34:53 -0400

    few changes

--- new/delay.py
+++ new/delay.py
@@ -2,11 +2,16 @@ import Tkinter
 import tkMessageBox
 from Sanghatest11 import *
 from astar_sid import *
+from random import randint
+from math import ceil
+import pygame
+
+pygame.init()
 
 initial_position=[0,0,3]
 goal_position=[11,11]
 print('\n\n')
-gridsize=20
+gridsize=30
 grid=backEndGrid(gridsize)
 print(grid)
 A4x4=[[300,400,100,0],[400,0,600,300],[800,200,900,100],[1800,1400,1200,600],[500,600,1000,1000],[500,1200,1000,1600]]
@@ -14,10 +19,12 @@ rect=[]
 list_line=[]
 obstacle_coord=[]
 snake_coord = []
-snake_size=4
+snake_size=2
+song = pygame.mixer.Sound('pacman_eatfruit.wav')
+
 
 #e is length of a square in the grid
-e=25
+e=30
 obstacle_list=[]
 class obs_cord():
     #diagonally opposite 2 points are sufficicent, nothing else needed
@@ -83,13 +90,18 @@ for xs1 in range (0,l1):
     objs_h[xs1].d=e*gridsize
     #obs(coord)
 #here i will try showing a grid which will guide my testing and initial coding
+grid_coord=[]
 def createGridVisible(l,objs,objs_h):
     in2=0
     for in2 in range(0,l):
-        id1 =C.create_line(objs[in2].a,objs[in2].b,objs[in2].c,objs[in2].d,fill="red")
-        id2 =C.create_line(objs_h[in2].a,objs_h[in2].b,objs_h[in2].c,objs_h[in2].d,fill="red")
+        grid_coord.append(C.create_line(objs[in2].a,objs[in2].b,objs[in2].c,objs[in2].d,fill="red"))
+        grid_coord.append(C.create_line(objs_h[in2].a,objs_h[in2].b,objs_h[in2].c,objs_h[in2].d,fill="red"))
 
     pass
+
+def drawGrid():
+    for grid in grid_coord:
+        C.itemconfig(grid,fill="red")
 
 def createVisibleObstacles(grid):
     lina=len(grid)
@@ -132,41 +144,93 @@ for any21 in obstacle_list:
 #here we have data from backend and graphics
 #this is the place where backend affects front ended
 #important note:- m up and down n right and left= mxn grid
+#=======================================================================================================================
+# keyboard events
+#=======================================================================================================================
+def on_key_press(event):
+    global paused, step, QUIT, drawVels
 
+    if event.keysym == "space":
+        paused = not paused
+    if event.keysym == "s":
+        step = True
+        paused = False
+    if event.keysym == "v":
+        drawVels = not drawVels
+    if event.keysym == "Escape":
+        QUIT = True
+
+
+apple=[]
 
 #yet to complete
 def generateApple():
     x,y=-1,-1
-    while grid[x][y]==1 or (x==-1 and y==-1):
-        x= randint(0,gridsize)
-        y= randint(0,gridsize)
+    while (x==-1 and y==-1) or grid[x][y]!=0 :
+        x= randint(0,gridsize-1)
+        y= randint(0,gridsize-1)
 
-    arc = C.create_oval(x+2,y+2,x+e-2,y+e-2,width=0,fill="black")
+    apple.append([x,y])
+    C.create_oval(x*e,y*e+2,(x+1)*e-2,(y+1)*e-2,width=0,fill="black")
     return x,y
 
-
 def regenerateApple():
-    C.itemconfig()
+    [x,y]=apple[-1]
+    C.create_oval(x*e,y*e+2,(x+1)*e-2,(y+1)*e-2,width=0,fill="black")
 
 top = Tkinter.Tk()
-top.wm_title("Snake_man Vs Ghost")
 counter=0
 coordinate=initial_position[0],initial_position[1],e,e
+
+# keyboard interaction
+top.bind("<space>",on_key_press)
+top.bind("s",on_key_press)
+top.bind("<Escape>",on_key_press)
+top.bind("v",on_key_press)
+
 C = Tkinter.Canvas(top, bg="gray", height=1200, width =1200)
 arc=C.create_arc(coordinate,start=30,extent=300,fill="red")
 angle=45
 close =1
+shut = 360
 flag1=True
 createVisibleObstacles(grid)
-createGridVisible(l,objs,objs_h)
+#createGridVisible(l,objs,objs_h)
 snake_coord.append(coordinate)
+#grid[coordinate[0]/e,coordinate[1]/e]=1
 
-#check if the grid has changed.
+staticgrid=grid
 
 
 def snake(coord,angle,close,flag12,l1,objs1,objs_h1,grid1,counter,snake_s,orientation):
 #    print orientation
+    global static
+    #staticgrid = grid1
     global shut
+    C.delete(Tkinter.ALL)
+
+
+    tail = 1
+    while tail<=snake_s:
+        if tail ==1: #,
+            arc = C.create_arc(snake_coord[-1],outline="gray",start=shut + angle,extent=(360-2*angle),fill="gray")
+    #        if (len(snake_coord)>int(e/2)):
+    #            arc = C.create_oval(snake_coord[-int(e/2)],width=0,fill="gray")
+        #    grid1[int(snake_coord[-1][1]/e),int(snake_coord[-1][0]/e)]=0
+    #    else:
+    #        if (len(snake_coord)>(tail-1)*e):
+    #            arc = C.create_oval(snake_coord[-((tail-1)*e)],width=0,fill="gray")
+    #        if (len(snake_coord)>(tail+1)*int(e)):
+    #            arc = C.create_oval(snake_coord[-((tail+1)*int(e/2))],width=0,fill="gray")
+    #            grid1[int(snake_coord[-((tail+1)*int(e))][1]/e),int(snake_coord[-((tail+1)*int(e))][0]/e)]=0
+        else:
+            if len(snake_coord)>(tail-1)*e - int(e/2):
+                arc = C.create_oval(snake_coord[-(tail-1)*e +int(e/2)],width=0,outline="gray",fill="gray")
+            if len(snake_coord)>(tail-1)*e:
+                arc = C.create_oval(snake_coord[-(tail-1)*e],width=0,outline="gray",fill="gray")
+
+        tail = tail+1
+    #drawGrid()
     if orientation == 3: #R
         a=1
         b=0
@@ -192,26 +256,14 @@ def snake(coord,angle,close,flag12,l1,objs1,objs_h1,grid1,counter,snake_s,orient
         d=-1
         shut = 90
 
-    tail = 1
-    while tail<=snake_s:
-        if tail ==1:
-            arc = C.create_arc(snake_coord[-1],outline="gray",start=shut + angle,extent=(360-2*angle),fill="gray")
-            if (len(snake_coord)>=int(e/2)):
-                arc = C.create_oval(snake_coord[-int(e/2)],width=0,fill="gray")
-        else:
-            if (len(snake_coord)>=(tail-1)*int(e)):
-                arc = C.create_oval(snake_coord[-((tail-1)*int(e))],width=0,fill="gray")
-            if (len(snake_coord)>=(tail+1)*int(e/2)):
-                arc = C.create_oval(snake_coord[-((tail+1)*int(e/2))],width=0,fill="gray")
-        tail = tail+1
-
-    if coord[1]>e*goal_position[1] or coord[1]<0 or coord[0]>e*goal_position[0] or coord[0]<0:
-        if coord[1]>e*goal_position[1] or coord[1]<0:
+    if coord[1]<0 or coord[0]<0:
+        if coord[1]<0:
             coord=coord[0]+1*a,coord[1],coord[2]+1*c,coord[3]
-        if coord[0]>e*goal_position[0] or coord[0]<0:
+        if coord[0]<0:
             coord=coord[0],coord[1]+1*b,coord[2],coord[3]+1*d
     else:
         coord=coord[0]+1*a,coord[1]+1*b,coord[2]+1*c,coord[3]+1*d
+
     if angle >0 and close ==1:
         angle=angle-5
         if angle ==0:
@@ -221,30 +273,63 @@ def snake(coord,angle,close,flag12,l1,objs1,objs_h1,grid1,counter,snake_s,orient
         if angle ==45:
             close=1
     snake_coord.append(coord)
+
     tail = 1
     while tail<=snake_s and len(snake_coord)>=tail*int(e):
         if tail ==1:
-            arc = C.create_arc(snake_coord[-1],start=shut + angle,extent= (360-2*angle),fill="red")
-            arc = C.create_oval(snake_coord[-int(e/2)],width=0,fill="red")
+           arc = C.create_arc(snake_coord[-1],outline="black",start=shut + angle,extent= (360-2*angle),fill="red")
+    #       if len(snake_coord)>int(e/2):
+    #            arc = C.create_oval(snake_coord[-int(e/2)],width=0,fill="red")
+    #        grid1[int(snake_coord[-1][1]/e),int(snake_coord[-1][0]/e)]=2
         else:
-        #    print -(tail*5)
-            arc = C.create_oval(snake_coord[-((tail-1)*int(e))],width=0,fill="red")
-            arc = C.create_oval(snake_coord[-((tail+1)*int(e/2))],width=0,fill="red")
+            if len(snake_coord)>(tail-1)*e - int(e/2):
+                arc = C.create_oval(snake_coord[-(tail-1)*e + int(e/2)],outline="black",width=0,fill="red")
+            if len(snake_coord)>(tail-1)*e:
+                arc = C.create_oval(snake_coord[-(tail-1)*e],outline="black",width=0,fill="red")
+
+        #        grid1[int(snake_coord[-((tail+1)*int(e))][1]/e),int(snake_coord[-((tail+1)*int(e))][0]/e)]=2
         tail = tail+1
 
     createGridVisible(l1,objs1,objs_h1)
-    drawObstacles()
+
+    createVisibleObstacles(grid1)
+
+    regenerateApple()
     #for coordinate2 in obstacle_coord:
     #    id = C.create_rectangle(coordinate2,fill="#000fff000" )
-    if coord[0]%e==goal_position[0] and coord[1]%e==goal_position[1]:
+    if coord[0]/e==goal_position[1] and coord[1]/e==goal_position[0]:
         print "Reached apple"
         snake_s = snake_s+1
+        song.play()
+
+        pathtotake=-1
+
+        while pathtotake==-1:
+            goal_position[1],goal_position[0]=generateApple()
+            pathtotake = astar_v2(initial_position,grid.tolist(),goal_position,gridsize)
+            #quit()
+        print goal_position
 
     if (coord[0]%e==0 and coord[1]%e==0):
-        pathtotake = astar_v2([coord[1]/e,coord[0]/e,orientation],grid.tolist(),goal_position)
-        top.after(30,lambda: snake(coord,angle,close,flag12,l1,objs1,objs_h1,grid1,counter,snake_s,pathtotake[1][2]))
+            #grid1[int(snake_coord[-12][1]/e),int(snake_coord[-12][0]/e)]=0
+
+            if len(snake_coord)>e*(snake_s):
+            #    print ceil(snake_coord[-e*(snake_s)][1]/e),ceil(snake_coord[-e*(snake_s)][0]/e)
+                #print snake_s
+                grid1[int(snake_coord[-e*(snake_s)-1][1]/e),int(snake_coord[-e*(snake_s)-1][0]/e)]=0
+
+
+    #    grid1[int(snake_coord[-6][1]/e),int(snake_coord[-6][0]/e)]=2
+
+        #    if len(snake_coord)>6:
+            grid1[ceil(snake_coord[-1][1]/e),ceil(snake_coord[-1][0]/e)]=2
+            #print grid1
+
+            pathtotake = astar_v2([coord[1]/e,coord[0]/e,orientation],grid1.tolist(),goal_position,gridsize)
+
+            top.after(10,lambda: snake(coord,angle,close,flag12,l1,objs1,objs_h1,grid1,counter,snake_s,pathtotake[1][2]))
     else:
-        top.after(30,lambda: snake(coord,angle,close,flag12,l1,objs1,objs_h1,grid1,counter,snake_s,orientation))
+        top.after(10,lambda: snake(coord,angle,close,flag12,l1,objs1,objs_h1,grid1,counter,snake_s,orientation))
 
 
 C.pack()
@@ -252,7 +337,13 @@ C.pack()
 # the main loop of the program
 
 #print grid.tolist()
-pathtotake = astar_v2(initial_position,grid.tolist(),goal_position)
+
+pathtotake=-1
+
+while pathtotake==-1:
+    goal_position[1],goal_position[0]=generateApple()
+    pathtotake = astar_v2(initial_position,grid.tolist(),goal_position,gridsize)
+print goal_position
 top.after(30,lambda: snake(coordinate,angle,close,flag1,l,objs,objs_h,grid,counter,snake_size,pathtotake[1][2]))
 '''
 while(flag1):
